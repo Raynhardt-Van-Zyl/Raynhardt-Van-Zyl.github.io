87f48905cba47a0a7528e227359ee3d40e1d4d42
Raynhardt Van Zyl <raynhardt.vanzyl@gmail.com>
2023-07-20 11:24:50 +0200

    sad
    asda

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Show "scroll to top" button when scrolling down
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#scrollTop').fadeIn();
+        } else {
+            $('#scrollTop').fadeOut();
+        }
+    });
+
+    // Scroll to top when the "scroll to top" button is clicked
+    $('#scrollTop').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
